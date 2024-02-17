45fbeb0549a12554622db63f0487fe40c3933a3b
Wajahat Ali <wajahatfaryadali@gmail.com>
2024-02-18 00:53:35 +0500

    create todo position set to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,26 @@
 import CustomLayout from "@/components/CustomLayout/CustomLayout";
 import CreateTodo from "@/components/TodoComponents/CreateTodo/CreateTodo";
-import { Grid } from "@mui/material";
+import { Box } from "@mui/material";
 // import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main>
       <CustomLayout>
-        <Grid container justifyContent={'center'}>
-          <CreateTodo />
-        </Grid>
+        <div style={{ position: 'relative' }}>
+          <Box component={'div'}
+            display={'flex'}
+            width={'100%'}
+            justifyContent={'center'}
+            position={"sticky"}
+            top={{ xs: '56px', sm: '64px' }}
+          >
+            <CreateTodo />
+          </Box>
+          <>
+          list
+          </>
+        </div>
       </CustomLayout>
     </main>
   );
--- src/components/CustomLayout/CustomLayout.tsx
+++ src/components/CustomLayout/CustomLayout.tsx
@@ -25,7 +25,6 @@ const CustomLayout: React.FC<CustomLayoutProps> = ({ children }) => {
                     <TopBar />
                     <Box mt={{ xs: '56px', sm: '64px' }} maxHeight={{ xs: 'calc(100vh - 56px)', sm: 'calc(100vh - 64px)' }}>
                         <Container sx={{ pt: 2 }}>
-
                             {children}
                         </Container>
                     </Box>
