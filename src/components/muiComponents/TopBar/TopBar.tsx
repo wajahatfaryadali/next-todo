fd82b76738d9433b87a53d59512d4c195114d6e1
Wajahat Ali <wajahatfaryadali@gmail.com>
2024-02-17 19:18:29 +0500

    top app bar added

---
+++ src/components/muiComponents/TopBar/TopBar.tsx
@@ -0,0 +1,44 @@
+import { Container } from '@mui/material';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Link from 'next/link';
+import classes from './topBar.module.css'
+import { URL_HOME, URL_SIGN_IN, URL_SIGN_UP } from '@/utils/routes-path';
+
+
+const TopBar = () => {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="fixed" classes={{ root: classes.appBar }}>
+                <Toolbar sx={{ px: { xs: 0, md: '' } }}>
+                    <Container
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'space-between'
+                        }}
+                    >
+                        <Link href={URL_SIGN_IN}>
+                            Todoz
+                        </Link>
+                        <Box sx={{ display: 'flex', gap: 2, flexGrow: 1, justifyContent: 'center' }}>
+                            <Link href={URL_HOME}>
+                                HOME
+                            </Link>
+                        </Box>
+                        <>
+                            <Link href={URL_SIGN_IN}>
+                                signIn
+                            </Link>
+                            <Link href={URL_SIGN_UP}>
+                                signUp
+                            </Link>
+                        </>
+                    </Container>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+}
+
+export default TopBar
