bd47a8054fcfbf4a1dd9dd9a97d0e4031f7fe36b
Wajahat Ali <wajahatfaryadali@gmail.com>
2024-02-17 18:09:39 +0500

    sign in component added

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,12 +1,84 @@
+'use client'
 import CustomLayout from '@/components/CustomLayout/CustomLayout'
 import BoxContainer from '@/components/muiComponents/BoxContainer/BoxContainer'
-import React from 'react'
+import { Box, Button, TextField, Typography } from '@mui/material'
+import React, { useState } from 'react'
+import Link from 'next/link'
+import { URL_SIGN_UP } from '@/utils/routes-path'
+
+
+interface SignInFormValueState {
+  email: string;
+  password: string | number;
+}
 
 const page = () => {
+
+  const [value, setValue] = useState<SignInFormValueState>({
+    email: '',
+    password: '',
+  })
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault()
+  }
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+    console.log('cehngae', event.target.id)
+    setValue({
+      ...value,
+      [event.target.id]: event.target.value
+    })
+  }
+
   return (
     <CustomLayout>
       <BoxContainer>
-        signin 
+        <Box
+          component={'form'}
+          onSubmit={handleSubmit}
+          display={'flex'}
+          flexDirection={'column'}
+          gap={2}
+        >
+          <Typography variant="h4" textAlign={'center'}>
+            Todo App
+          </Typography>
+          <Typography variant="h5" textAlign={'center'}>
+            Sign In
+          </Typography>
+          <TextField
+            id="email"
+            label="Email"
+            type="email"
+            value={value.email}
+            onChange={handleChange}
+            required
+          />
+          <TextField
+            id="password"
+            label="Password"
+            type="password"
+            value={value.password}
+            onChange={handleChange}
+            required
+            sx={{ color: '#fff' }}
+          />
+
+          <Button variant='contained' fullWidth type='submit'>
+            Log in
+          </Button>
+          <Box>
+            <Typography color={'primary.textGray'}>
+              Don't have an account? {' '}
+              <Link href={URL_SIGN_UP}>
+                <Typography component={'span'} color={'primary.light'} sx={{ cursor: 'pointer' }} fontWeight={500}>
+                  Sign Up.
+                </Typography>
+              </Link>
+            </Typography>
+          </Box>
+        </Box>
       </BoxContainer>
     </CustomLayout>
   )
